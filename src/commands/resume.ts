c874a2cf58a8c06d507758fc1c2284ceb1020ab5
Diego Tsuyoshi <diego@tsuyoshi.com.br>
2024-02-12 14:18:45 -0400

    feat: pause/resume command

---
+++ src/commands/resume.ts
@@ -0,0 +1,30 @@
+import { Message } from "discord.js";
+import Command from ".";
+import HappyClient from "../client";
+import Player from "../modules/music/player";
+
+export default class Resume extends Command {
+  prefix = '!resume';
+  description = 'Resume the current song. Usage: !resume';
+
+  constructor(
+    public readonly client: HappyClient,
+    private readonly player: Player
+    ) {
+    super(client);
+  }
+
+  async execute (message: Message) {
+    if (!message.member) return
+
+    const voiceChannel = message.member.voice.channel;
+    const isInDifferentChannel = this.client.connection?.joinConfig.channelId !== voiceChannel?.id;
+
+    if (isInDifferentChannel) {
+      return message.reply(
+        "You need to be in the same voice channel as me to resume the song.");
+    }
+
+    this.player.resume();
+  }
+}
