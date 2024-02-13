21149ba22f444ee3e74dddbc35452ce5294e76c4
Diego Tsuyoshi <diego@tsuyoshi.com.br>
2024-02-13 15:52:34 -0400

    refactor: P command to use new ConnectionManager system

--- src/commands/p.ts
+++ src/commands/p.ts
@@ -2,10 +2,8 @@ import crypto from "crypto";
 import { Message } from "discord.js";
 import Command from ".";
 import HappyClient from "../client";
-import Player from "../modules/music/player";
-import Queue from "../modules/music/queue";
-import SpotifyClient from "../modules/music/spotify";
-import YoutubeSource, { SearchResult } from "../modules/music/youtube";
+import { SearchResult } from "../modules/music/youtube";
+import ConnectionManager from "../connection-manager";
 
 export default class P extends Command {
   prefix = "!p";
@@ -13,54 +11,51 @@ export default class P extends Command {
 
   constructor(
     public readonly client: HappyClient,
-    private readonly youtube: YoutubeSource,
-    private readonly queue: Queue,
-    private readonly player: Player,
-    private readonly spotify: SpotifyClient,
+    private readonly connectionManager: ConnectionManager
   ) {
     super();
   }
 
   async execute(message: Message) {
-    if (!message.member) return;
+    if (!message.member || !message.guild) return;
 
-    const voiceChannel = message.member.voice.channel;
-    const isInDifferentChannel = this.client.connection?.joinConfig.channelId !== voiceChannel?.id;
-
-    if (!voiceChannel) {
+    if (!message.member.voice.channel) {
       return message.reply("Please join a voice channel to add a song to the queue.");
     }
 
-    if (this.client.connection && isInDifferentChannel) {
+    let connection = this.connectionManager.getConnection(message);
+
+    if (connection && !this.connectionManager.isInSameChannel(message)) {
       return message.reply("You need to be in the same voice channel as the bot to add a song to the queue.");
     }
 
-    // !p gossip maneskin
-    // ['!p ', 'gossip maneskin']
+
+    if (!connection) {
+      connection = this.connectionManager.joinVoiceChannel({
+        channelId: message.member.voice.channel.id,
+        guildId: message.guild.id,
+        adapterCreator: message.guild.voiceAdapterCreator,
+      });
+    }
+
     const [, search] = message.content.split("!p ");
 
-    const connection = this.client.joinVoiceChannel({
-      channelId: voiceChannel.id,
-      guildId: voiceChannel.guild.id,
-      adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-    });
-
-    this.player.connect(connection);
+    connection.player.connect(connection?.voiceConnection);
 
     let youtubeSearch: SearchResult | null = null;
 
-    if (this.spotify.isPlaylistUrl(search)) {
-      const tracks = await this.spotify.getTracks(search);
+    if (connection.spotify.isPlaylistUrl(search)) {
+      const tracks = await connection.spotify.getTracks(search);
 
       if (!tracks.length) {
         return message.reply("No playlist found.");
       }
 
-      youtubeSearch = (await Promise.all(tracks.map((track) => this.youtube.search({ search: track.title }))))
+      youtubeSearch = (await Promise.all(tracks.map((track) => connection?.youtube.search({ search: track.title }))))
         .flat()
         .filter(Boolean) as SearchResult;
     } else {
-      youtubeSearch = await this.youtube.search({ search: search });
+      youtubeSearch = await connection.youtube.search({ search: search });
     }
 
     if (!youtubeSearch?.length) {
@@ -74,10 +69,10 @@ export default class P extends Command {
       fileName: crypto.randomUUID(),
     }));
 
-    await this.youtube.download(songs[0]);
+    await connection.youtube.download(songs[0]);
 
-    this.queue.add(songs);
+    connection.queue.add(songs);
 
-    this.player.play();
+    connection.player.play();
   }
 }
