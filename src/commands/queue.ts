d426aa07810ae77635e8ffbf00fd1aa38dc9c42c
Diego Tsuyoshi <diego@tsuyoshi.com.br>
2024-02-13 07:13:37 -0400

    refactor: queue discord message

--- src/commands/queue.ts
+++ src/commands/queue.ts
@@ -25,18 +25,8 @@ export default class Queue extends Command {
     }
 
 
-    let queueMessage = `**Now Playing:**\n${currentSong.title} - Requested by <@${currentSong.requestedBy}>\n\n**Queue:**\n`
 
-    if (queue.length === 0) {
-      queueMessage += 'The queue is empty.'
 
-      return message.reply(queueMessage);
-    }
-
-    for (let i = 0; i < queue.length; i++) {
-      queueMessage += `${i}. ${queue[i].title} - Requested by <@${queue[i].requestedBy}>\n`
-    }
-
-    message.reply(queueMessage);
+    message.reply(this.queue.toDiscordMessage());
   }
 }
